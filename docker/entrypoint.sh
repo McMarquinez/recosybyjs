cd58a7a8e2b76eb9587f1d8881ca94792697d90c
MC Marquinez <mcmarquinez01@gmail.com>
2026-04-13 16:23:30 +0800

    Remove env check in dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-# Ensure .env exists
-if [ ! -f .env ] && [ -f .env.production ]; then
-  cp .env.production .env
-fi
-
-# Generate key if missing
-if ! grep -q "APP_KEY=" .env || [ -z "$(grep APP_KEY .env | cut -d '=' -f2)" ]; then
-  php artisan key:generate
-fi
-
 php artisan storage:link || true
 
 php artisan config:clear
@@ -21,4 +11,4 @@ if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
   php artisan migrate --force
 fi
 
-exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
+exec php -S 0.0.0.0:${PORT:-10000} -t public
